97e256c1737c374a5bc716c44109d6189998a554
Prototyype <98288464+colecantu904@users.noreply.github.com>
2023-08-09 15:47:20 -0500

    enemy raycast

--- invasion/Assets/enemyScript.cs
+++ invasion/Assets/enemyScript.cs
@@ -8,7 +8,8 @@ public class enemyScript : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] public int health = 5;
     [SerializeField] public float delay = 1f;
-    private bool activated = false;
+    [SerializeField] float range = 1;
+    [SerializeField] public Transform raycastStart;
 
     public void takeDamage()
     {
@@ -33,11 +34,6 @@ public class enemyScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player" && !activated)
-        {
-            setTargettoPlayer();
-        }
-
         if (collision.tag == "patrolTarget")
         {
             collision.GetComponent<patrolTargetScript>().rotateTarget();
@@ -47,9 +43,26 @@ public class enemyScript : MonoBehaviour
     void setTargettoPlayer()
     {
         var enemy = GetComponent<enemyMove>();
-        //enemy.enabled = true;
         enemy.target = GameObject.Find("Player").GetComponent<Transform>();
         GetComponent<enemyMove>().pathRate = .5f;
     }
+    private void detectPlayer()
+    {
+        var hit = Physics2D.Raycast(raycastStart.position, raycastStart.up*range);
+        
+        if (hit.collider != null && !hit.collider.isTrigger&&hit.collider.tag=="Player")
+        {
 
+            setTargettoPlayer();
+            Debug.Log("found");
+        }
+    }
+    private void Update()
+    {
+        detectPlayer();
+    }
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(raycastStart.position, raycastStart.up*range,Color.black);
+    }
 }
